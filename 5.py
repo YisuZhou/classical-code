81d65ba1e4673aaacfa91aaae99e7d11423e265e
YisuZhou <35996961+YisuZhou@users.noreply.github.com>
2019-07-09 17:56:57 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -9,7 +9,7 @@
 l_map = {}  # 存储字典 列表元素:包含该元素的最长序列长度
 l_max = 1
 for i in l:
-    if i not in l_map:
+    if i not in l_map:  # 避免重复元素影响
         # 确定左右连续串长度
         if i-1 not in l_map:
             i_left = 0
@@ -30,5 +30,25 @@ for i in l:
         l_max = max(l_max,l_map[i])
 print(l_max)
 
+# 看到一种新解法，二进制用处很多。二进制下，在对应数字的位置置1，然后列表遍历结束后，会得到一个数。最后问题转变成，某个数的二进制下有几个连续的1
+def solution(line):
+    data = line.split(",")
+    num = 0
+    # 在对应的位置1
+    for x in data:
+        num = num|(1<<int(x)) 
+    # 获取二进制字符串
+    num = bin(num)[2:]  # bin()返回的是字符串，且开头是0b
+    # 获得最大连续的1数量
+    continuous = 0
+    maxcontinuous = 0
+    for x in num:
+        if x == '0':
+            if continuous > maxcontinuous:
+                maxcontinuous = continuous
+            continuous = 0
+        else:
+            continuous += 1
+    return str(maxcontinuous)
 
 
