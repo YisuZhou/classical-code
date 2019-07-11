465e9630e540f81780ce756eb14d32ade140cd5a
YisuZhou <35996961+YisuZhou@users.noreply.github.com>
2019-07-11 15:46:50 +0800

    Update 9 .py

--- 9 .py
+++ 9 .py
@@ -1,1 +1,28 @@
+# 中序遍历，递归版
+def mid(root):
+    if !root:
+        return
+    mid(root.left)
+    print(root.val)
+    mid(root.right)
+   
+  
+# 中序遍历，非递归版。和前序非递归一样，用栈储存每个父节点
+def mid(root):
+    if !root:
+        return
+    root_list = []
+    cur_r = root
+    
+    while cur_r or root_list:
+        while cur_r:
+            root_list.append(cur_r)
+            cur_r = cur_r.left
+        # 以上就把所有的左支存下来了
+        
+        if root_list:  # 下面有pop，记得要这个if条件
+            cur_r = root_list.pop()
+            print(cur_r.val)
+            # 以上就把左和根打出来了
+            cur_r = cur_r.right
 
